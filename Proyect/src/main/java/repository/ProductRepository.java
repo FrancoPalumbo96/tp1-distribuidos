adb578d94ba50450e4dbc4822ab731eece5ee70d
agustinmasuelli <agus.masu@gmail.com>
2019-04-08 16:41:23 -0300

    Changes to adapt to new Models

--- Proyect/src/main/java/repository/ProductRepository.java
+++ Proyect/src/main/java/repository/ProductRepository.java
@@ -17,7 +17,7 @@ public class ProductRepository extends BaseRepository<PersistProduct> {
     @Override
     public PersistProduct getById(Long id) throws NotFoundException {
         super.refreshSession();
-        PersistProduct product = (PersistProduct) session.get(PersistProduct.class, id);
+        PersistProduct product = session.get(PersistProduct.class, id);
         if (product == null) throw new NotFoundException("Product Not Found");
         else return product;
     }
--- Proyect/src/main/java/services/ProductServiceImpl.java
+++ Proyect/src/main/java/services/ProductServiceImpl.java
@@ -1,10 +1,7 @@
 package services;
 
 
-import com.productCatalog.Product;
-import com.productCatalog.ProductID;
-import com.productCatalog.ProductServiceGrpc;
-import com.productCatalog.User;
+import com.productCatalog.*;
 import io.grpc.stub.StreamObserver;
 import javassist.NotFoundException;
 import model.persist.product.PersistProduct;
@@ -17,31 +14,16 @@ public class ProductServiceImpl extends ProductServiceGrpc.ProductServiceImplBas
     final ProductRepository productRepository = ProductRepository.getInstance();
 
     @Override
-    public void addProduct(Product request, StreamObserver<Product> responseObserver) {
-        final PersistProduct toPersist = ProductFactory.createPersistProductFromProduct(request);
-        this.productRepository.save(toPersist);
-        responseObserver.onNext(request);
+    public void addProduct(CreateProductRequest request, StreamObserver<Product> responseObserver) {
+        final PersistProduct toSave = new PersistProduct(request.getName(), request.getPrice(), request.getDescription());
+        this.productRepository.save(toSave);
+        Product product = ProductFactory.createProuctFromPersistProduct(toSave);
+        responseObserver.onNext(product);
         responseObserver.onCompleted();
     }
 
     @Override
-    public void getWishlist(User request, StreamObserver<Product> responseObserver) {
-        super.getWishlist(request, responseObserver);
-    }
-
-    @Override
-    public void deleteProductFromWishlist(Product request, StreamObserver<Product> responseObserver) {
-        super.deleteProductFromWishlist(request, responseObserver);
-    }
-
-    @Override
     public void getProductInfo(ProductID request, StreamObserver<Product> responseObserver) {
-        if (request.getId() == 1L){
-            responseObserver.onNext(Product.newBuilder().setId(1).setPrice(0.2f).setName("Masu").setDescription("Masu").build());
-            responseObserver.onCompleted();
-            return;
-        }
-
         try {
             final PersistProduct persistProduct = this.productRepository.getById(request.getId());
             final Product product = ProductFactory.createProuctFromPersistProduct(persistProduct);
